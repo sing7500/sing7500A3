ffe1461fa728e9d6dab1bf7d4cba529c2ad3a777
sing7500 <52579783+sing7500@users.noreply.github.com>
2019-07-28 18:10:47 -0400

    Add files via upload

---
+++ assets/Data/mySongs.ts
@@ -0,0 +1,8 @@
+import {SongInfo} from '../../app/songInfo';
+
+export const MYSONGS:SongInfo[]=[
+    {songName:'Zindagi',artistName:'Sharry Mann',genre:'Classical',yearReleased:'2019',picture:'./assets/images/zindagi.jpg'},
+    {songName:'ASOOL',artistName:'Tarsem Jassar',genre:'Popular',yearReleased:'2016',picture:'./assets/images/Asool.jpg'},
+    {songName:'Patiala Shahi Pagg',artistName:'Kulbir Jhinjer',genre:'Folk',yearReleased:'2014',picture:'./assets/images/Pagg.jpg'},
+    {songName:' Ik Tare Wala',artistName:'Ranjit Bawa',genre:'Classical',yearReleased:'2018',picture:'./assets/images/Ik.jpg'}
+];
